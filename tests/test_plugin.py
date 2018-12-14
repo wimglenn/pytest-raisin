0fa77926771f378d2b5364b6d667b495d04562d8
wim glenn <wim.glenn@gmail.com>
2018-12-14 16:57:17 -0600

    v0.1 tests

---
+++ tests/test_plugin.py
@@ -0,0 +1,102 @@
+pytest_plugins = ["pytester"]
+
+
+def test_using_exception_class(testdir):
+    testdir.makepyfile("""
+        import pytest
+
+        def test_name_error_class():
+            with pytest.raises(NameError):
+                wtf
+    """)
+    result = testdir.runpytest()
+    result.assert_outcomes(passed=1)
+
+
+def test_using_exception_instance(testdir):
+    testdir.makepyfile("""
+        import pytest
+
+        def test_name_error_instance():
+            with pytest.raises(IndexError("list index out of range")):
+                [][0]
+    """)
+    result = testdir.runpytest()
+    result.assert_outcomes(passed=1)
+
+
+def test_using_exception_instance_wrong_args(testdir):
+    testdir.makepyfile("""
+        import pytest
+
+        def test_index_error_instance():
+            with pytest.raises(IndexError("Hello, I am a potato")):
+                [][0]
+    """)
+    result = testdir.runpytest()
+    result.assert_outcomes(failed=1)
+    result.stdout.fnmatch_lines([
+        "test_using_exception_instance_wrong_args.py F *",
+        "E * AssertionError: IndexError args do not match!",
+        "E *   Actual:   ('list index out of range',)",
+        "E *   Expected: ('Hello, I am a potato',)",
+    ])
+
+
+def test_using_exception_instance_wrong_type(testdir):
+    testdir.makepyfile("""
+        import pytest
+
+        def test_index_error_instance():
+            with pytest.raises(LookupError("list index out of range")):
+                [][0]
+    """)
+    result = testdir.runpytest()
+    result.assert_outcomes(failed=1)
+    result.stdout.fnmatch_lines([
+        "test_using_exception_instance_wrong_type.py F *",
+        "test_using_exception_instance_wrong_type.py:5: IndexError",
+    ])
+
+
+def test_failure_to_raise_uses_full_repr(testdir):
+    testdir.makepyfile("""
+        import pytest
+
+        def test_no_raise():
+            with pytest.raises(SystemExit(2, 'bye')):
+                pass
+    """)
+    result = testdir.runpytest()
+    result.assert_outcomes(failed=1)
+    result.stdout.fnmatch_lines([
+        "test_failure_to_raise_uses_full_repr.py F *",
+        "E * Failed: DID NOT RAISE SystemExit(2, 'bye')",
+    ])
+
+
+def test_using_exception_instance_and_regex_matches(testdir):
+    testdir.makepyfile("""
+        import pytest
+
+        def test_name_error_instance():
+            with pytest.raises(Exception("somebody set up us the bomb"), match="bomb"):
+                raise Exception("somebody set up us the bomb")
+    """)
+    result = testdir.runpytest()
+    result.assert_outcomes(passed=1)
+
+
+def test_using_exception_instance_and_regex_doesnt_match(testdir):
+    testdir.makepyfile("""
+        import pytest
+
+        def test_name_error_instance():
+            with pytest.raises(Exception("somebody set up us the bomb"), match="2101"):
+                raise Exception("somebody set up us the bomb")
+    """)
+    result = testdir.runpytest()
+    result.assert_outcomes(failed=1)
+    result.stdout.fnmatch_lines([
+        "E * AssertionError: Pattern '2101' not found in 'somebody set up us the bomb'"
+    ])
