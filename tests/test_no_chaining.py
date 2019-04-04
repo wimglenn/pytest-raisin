44e4eebb78e9bbd3bd9a5fbc3cb2ab2ccd2ea10b
wim glenn <hey@wimglenn.com>
2019-04-03 22:36:15 -0500

    Use a cross-compat trick to squash chaining, stolen from pyparsing.py (thanks PaulMcG)

---
+++ tests/test_no_chaining.py
@@ -0,0 +1,20 @@
+pytest_plugins = ["pytester", "pytest-raisin"]
+
+
+def test_spurious_chaining_is_suppressed(testdir):
+    testdir.makepyfile("""
+        import pytest
+
+        def test_the_thing():
+            with pytest.raises(Exception("boom")):
+                raise Exception("bang")
+    """)
+    result = testdir.runpytest()
+    result.assert_outcomes(failed=1)
+    result.stdout.fnmatch_lines([
+        "E * AssertionError: Exception args do not match!",
+        "E *   Actual:   ('bang',)",
+        "E *   Expected: ('boom',)",
+    ])
+    text = result.stdout.str()
+    assert "During handling of the above exception, another exception occurred" not in text
